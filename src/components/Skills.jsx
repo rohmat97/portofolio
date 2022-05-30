e342bbe06c08608a41bdb7bf707d7476677cde67
Rohmat PC <rohmat661@gmail.com>
2022-05-31 06:36:07 +0700

    fixing bugs responsive

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,7 +18,7 @@ import SDK from '../assets/sdk.png';
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300 ">
+    <div name="skills" className="w-full h-full h-screen bg-[#0a192f] text-gray-300 ">
         {/* container */}
         <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
             <div className="">
