2e34ebec065b81a4ac162bc80b0717a8933fe44a
Rohmat PC <rohmat661@gmail.com>
2022-06-05 01:00:01 +0700

    update profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import Link from 'react-scroll/modules/components/Link';
+import Profile from '../assets/Profile.jpg';
 const Home = () => {
   return (
     <div className=' w-full h-screen bg-[#0a192f]' name="home">
         {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-pink-600'>Hi, my name is</p>
-            <h1 className=' text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Rohmat Dasuki</h1>
-            <h2 className=' text-4xl sm:text-7xl font-bold text-[#8892b0] '>I'm a Full Stack Developer.</h2>
-            <p className=' text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer who specializes in building (and sometimes
-             designing) a great digital experience. Right now, I'm focusing on
-             build mobile apps using React-Native, but if needed web or back-end, i'm ready on it.</p>
-          <div>
-              <button className=' text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                  View Work 
-                  <span className=' group-hover:rotate-90 duration-300'>
-                    <HiArrowNarrowRight className=' ml-3' />
-                  </span>
-              </button>
-          </div>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-row justify-center h-full'>
+          
+            <img className="w-64 h-64 m-auto" src={Profile} alt="Profile" />
+            <div className='mx-auto px-8 flex flex-col justify-center h-full pt-20'>
+              <p className='text-pink-600'>Hi, my name is</p>
+              <h1 className=' text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Rohmat Dasuki</h1>
+              <h2 className=' text-4xl sm:text-7xl font-bold text-[#8892b0] '>I'm a Full Stack Developer.</h2>
+              <p className=' text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer who specializes in building (and sometimes
+              designing) a great digital experience. Right now, I'm focusing on
+              build mobile apps using React-Native, but if needed web or back-end, i'm ready on it.</p>
+              <div>
+                <Link to='about' smooth={true} duration={500}>
+                  <button className=' text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 rounded-2xl'>
+                    View More 
+                  </button>
+                </Link>
+            </div>
+            </div>
         </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Logo from '../assets/logo.png';
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook  } from 'react-icons/fa';
+import { FaBars, FaTimes, FaGithub, FaLinkedin  } from 'react-icons/fa';
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
@@ -30,11 +30,11 @@ const NavBar = () => {
             Skills
           </Link>
         </li>
-        {/* <li>
-          <Link to='work' smooth={true} duration={500}>
-            Work
+        <li>
+          <Link to='journey' smooth={true} duration={500}>
+            Journey
           </Link>
-        </li> */}
+        </li>
         <li>
           <Link to='contact' smooth={true} duration={500}>
             Contact
