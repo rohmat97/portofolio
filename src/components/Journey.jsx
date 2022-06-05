736f38c135f85df2ab7a37f48fd330fe1c8e8943
Rohmat PC <rohmat661@gmail.com>
2022-06-06 06:01:49 +0700

    update styles

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -73,7 +73,7 @@ const Journey = () => {
     <div name="journey" className=' w-full h-full bg-[#0a192f] text-gray-300 py-32'>
         <div className=' flex flex-col justify-center items-center w-full h-full'>
             <div className="max-w-[1000px] w-full px-4 grid gap-8">
-                <div className="sm:text-left pb-8 pl-4 pl-[37.5%]">
+                <div className="sm:text-left pb-8 pl-4">
                     <p className="text-4xl font-bold inline border-b-4 border-pink-600">Journey</p>
                     <p className="pt-4">// There are the companies I've worked</p>
                 </div>
