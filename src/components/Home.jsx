78b453a4754e6c98a165d502ab75df672503f9a7
Rohmat PC <rohmat661@gmail.com>
2022-06-06 06:11:29 +0700

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
     <div className=' w-full h-screen bg-[#0a192f]' name="home">
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-row justify-center h-full'>
-          
             <img className="w-64 h-64 m-auto" src={Profile} alt="Profile" />
             <div className='mx-auto px-8 flex flex-col justify-center h-full pt-20'>
               <p className='text-pink-600'>Hi, my name is</p>
